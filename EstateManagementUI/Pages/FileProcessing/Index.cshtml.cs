013702f30771cb441ad820d92d77cf63a75a481d
Stuart Ferguson <stuart_ferguson1@outlook.com>
2024-07-10 22:42:22 +0100

    create default landing page and application start point

---
+++ EstateManagementUI/Pages/FileProcessing/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EstateManagementUI.Pages.FileProcessing
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EstateManagementUI/Pages/Merchant/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EstateManagementUI.Pages.Merchant
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
