ef87e9c353d2f806bd5c922e139c6107ebba32c2
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2024-10-03 16:46:22 +0100

    make permission on drop down a tuple

--- EstateManagementUI/Pages/Shared/TableDropdownItem.cs
+++ EstateManagementUI/Pages/Shared/TableDropdownItem.cs
@@ -2,4 +2,4 @@ using System.Linq.Expressions;
 
 namespace EstateManagementUI.Pages.Shared;
 
-public record TableDropdownItem(string Text, Expression<Func<Task>> Action, String applicationSection, String function,String Id = null);
+public record TableDropdownItem(string Text, Expression<Func<Task>> Action, (String applicationSection, String function) permission,String Id = null);
