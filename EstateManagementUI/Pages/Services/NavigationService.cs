ebcdc26889760822338cc0186dcaa03525a1eedf
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2025-01-01 22:58:51 +0000

    Refactor hosts file handling and improve navigation rendering
    - Updated `DockerHelper.cs` to use a variable for the hosts file path, enhancing readability and maintainability.
    - Reformatted code in `NavigationService.cs` for better clarity, including changes to the rendering of navigation items using an anchor tag.
    - Modified `_Navigation.cshtml` to use `@Html.Raw(...)` for rendering navigation items, ensuring proper HTML interpretation in the Razor view.

--- EstateManagementUI/Pages/Services/NavigationService.cs
+++ EstateManagementUI/Pages/Services/NavigationService.cs
@@ -30,35 +30,37 @@ namespace EstateManagementUI.Pages.Services
 
         public async Task<string> RenderItem(string userName, string title, string name, string id, string pageName = "Index")
         {
-            Result permissionResult = await this.PermissionsService.DoIHavePermissions(userName, name);
-            if (permissionResult.IsFailed)
-            {
-                Logger.LogWarning(permissionResult.Message);
-                return string.Empty;
-            }
-            else
-            {
-                Logger.LogWarning($"Permission granted for userName: {userName} SectionName: {name}");
-            }
+        Result permissionResult = await this.PermissionsService.DoIHavePermissions(userName, name);
+        if (permissionResult.IsFailed)
+        {
+            Logger.LogWarning(permissionResult.Message);
+            return string.Empty;
+        }
+        else
+        {
+            Logger.LogWarning($"Permission granted for userName: {userName} SectionName: {name}");
+        }
 
-            var link = $"/{name}/{pageName}";
-            var icon = name switch
-            {
-                "Estate" => "fa-solid fa-network-wired",
-                "Merchant" => "fa-solid fa-store",
-                "Contract" => "fa-solid fa-file-contract",
-                "Operator" => "fa-solid fa-building-columns",
-                "Reporting" => "fa-solid fa-chart-simple",
-                "File Processing" => "fa-solid fa-file-csv",
-                _ => "fa-solid fa-gauge-high"
-            };
 
-            return $@"
-                <a class='nav-link' asp-page='{link}' id='{id}'>
-                    <i class='{icon}'></i>
-                    <partial name='Loading'/>
-                    <p>{title}</p>
-                </a>";
+
+        var link = $"/{name}/{pageName}";
+        var icon = name switch
+        {
+            "Estate" => "fa-solid fa-network-wired",
+            "Merchant" => "fa-solid fa-store",
+            "Contract" => "fa-solid fa-file-contract",
+            "Operator" => "fa-solid fa-building-columns",
+            "Reporting" => "fa-solid fa-chart-simple",
+            "File Processing" => "fa-solid fa-file-csv",
+            _ => "fa-solid fa-gauge-high"
+        };
+
+        return $@"
+            <a class='nav-link' href='{link}' id='{id}'>
+                <i class='{icon}'></i>
+                <partial name='Loading'/>
+                <p>{title}</p>
+            </a>";
         }
     }
 }
